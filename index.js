6cc4d0123f1791540b2d8b64f08759c5916fc16f
unknown <batyrkhand@gmail.com>
2023-04-11 15:22:47 +0600

    Genre feature added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const booksRoutes = require('./routes/books.js');
 const authorsRoutes = require('./routes/authors.js');
+const genreRoutes = require('./routes/genre.js');
 
 const app = express();
-
 const dbMongo = 'mongodb://localhost:27017/admin';
 const mongoose = require('mongoose');
+
 mongoose.connect(dbMongo)
     .then((result) => console.log("Success MongoDB"))
     .catch((err) => console.log(err));
@@ -18,6 +20,8 @@ app.use(bodyParser.json());
 app.use('/books', booksRoutes);
 
 app.use('/authors', authorsRoutes);
+
+app.use('/genre', genreRoutes)
 
 app.get('/', (req, res) => {
     console.log("GET '/' rout");
---
+++ models/genre.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const genreSchema = new Schema(
+    {
+        genre : {
+            type : String,
+            required : true
+        }
+    },{timestamps : true});
+
+const Genre = mongoose.model('Genre', genreSchema);
+module.exports = Genre;
